8a6083dceccbd984b905936bc81a8ac0aa56de3a
Alex Vergara <pyzyryab@tutanota.com>
2022-08-14 15:53:07 +0200

    Restructured base crates organization. Project renamed to canyon_fwk. Added macro crates

--- tests/server.rs
+++
@@ -1,8 +0,0 @@
-use canyon;
-
-/// Tests if the Canyon http server it's set up correctly
-#[test]
-fn canyon_fire() {
-    // TODO Spawn on a thread and make a call through the loopback to test the behaviour
-    canyon::fire();
-}
